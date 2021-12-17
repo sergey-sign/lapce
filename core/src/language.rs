7da39dc7a15f24833060cb670702af1dd9d88ee8
Sergey Sign <sergey.sign@gmail.com>
2021-12-17 12:35:20 +0200

    enable ruby syntax

--- core/src/language.rs
+++ core/src/language.rs
@@ -1,22 +1,36 @@
 use std::str::FromStr;
 use std::{collections::HashMap, path::PathBuf};
 use tree_sitter::Parser;
-use tree_sitter_highlight::HighlightConfiguration;
-use tree_sitter_rust;
 use tree_sitter_go;
+use tree_sitter_highlight::HighlightConfiguration;
+use tree_sitter_ruby;
+use tree_sitter_rust;
 
 #[derive(Eq, PartialEq, Hash, Copy, Clone)]
 pub enum LapceLanguage {
     Rust,
     Go,
+    Ruby,
 }
 
 impl LapceLanguage {
     pub fn from_path(path: &PathBuf) -> Option<LapceLanguage> {
-        let extension = path.extension()?.to_str()?;
+        let ext = path.extension();
+        if ext == None {
+            let file_name = path.file_stem()?.to_str()?;
+            return match file_name {
+                "Gemfile" | "Guardfile" | "Rakefile" | "gemspec" | "Procfile" => {
+                    Some(LapceLanguage::Ruby)
+                }
+                _ => None,
+            };
+        }
+
+        let extension = ext?.to_str()?;
         Some(match extension {
             "rs" => LapceLanguage::Rust,
             "go" => LapceLanguage::Go,
+            "rb" | "rake" | "ru" => LapceLanguage::Ruby,
             _ => return None,
         })
     }
@@ -68,44 +82,80 @@ pub fn new_highlight_config(
 
             (configuration, recognized_names)
         }
-            LapceLanguage::Go => {
-                let mut configuration = HighlightConfiguration::new(
-                    tree_sitter_go::language(),
-                    tree_sitter_go::HIGHLIGHT_QUERY,
-                    "",
-                    "",
-                )
-                .unwrap();
-                let recognized_names = vec![
-                    "constant",
-                    "constant.builtin",
-                    "type",
-                    "type.builtin",
-                    "property",
-                    "comment",
-                    "constructor",
-                    "function",
-                    "function.method",
-                    "function.macro",
-                    "punctuation.bracket",
-                    "punctuation.delimiter",
-                    "label",
-                    "keyword",
-                    "string",
-                    "variable.parameter",
-                    "variable.builtin",
-                    "operator",
-                    "attribute",
-                    "escape",
-                ]
-                .iter()
-                .map(|s| s.to_string())
-                .collect::<Vec<String>>();
-                configuration.configure(&recognized_names);
+        LapceLanguage::Go => {
+            let mut configuration = HighlightConfiguration::new(
+                tree_sitter_go::language(),
+                tree_sitter_go::HIGHLIGHT_QUERY,
+                "",
+                "",
+            )
+            .unwrap();
+            let recognized_names = vec![
+                "constant",
+                "constant.builtin",
+                "type",
+                "type.builtin",
+                "property",
+                "comment",
+                "constructor",
+                "function",
+                "function.method",
+                "function.macro",
+                "punctuation.bracket",
+                "punctuation.delimiter",
+                "label",
+                "keyword",
+                "string",
+                "variable.parameter",
+                "variable.builtin",
+                "operator",
+                "attribute",
+                "escape",
+            ]
+            .iter()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
+            configuration.configure(&recognized_names);
 
-                (configuration, recognized_names)
-            }
+            (configuration, recognized_names)
+        }
+        LapceLanguage::Ruby => {
+            let mut configuration = HighlightConfiguration::new(
+                tree_sitter_ruby::language(),
+                tree_sitter_ruby::HIGHLIGHT_QUERY,
+                "",
+                "",
+            )
+            .unwrap();
+            let recognized_names = vec![
+                "constant",
+                "constant.builtin",
+                "type",
+                "type.builtin",
+                "property",
+                "comment",
+                "constructor",
+                "function",
+                "function.method",
+                "function.macro",
+                "punctuation.bracket",
+                "punctuation.delimiter",
+                "label",
+                "keyword",
+                "string",
+                "variable.parameter",
+                "variable.builtin",
+                "operator",
+                "attribute",
+                "escape",
+            ]
+            .iter()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
+            configuration.configure(&recognized_names);
 
+            (configuration, recognized_names)
+        }
     }
 }
 
@@ -113,6 +163,7 @@ pub fn new_parser(language: LapceLanguage) -> Parser {
     let language = match language {
         LapceLanguage::Rust => tree_sitter_rust::language(),
         LapceLanguage::Go => tree_sitter_go::language(),
+        LapceLanguage::Ruby => tree_sitter_ruby::language(),
     };
     let mut parser = Parser::new();
     parser.set_language(language).unwrap();
